0e09cabe533379bdefbc151ea8ec00cfce8440c4
lbarosi <lbarosi@df.ufcg.edu.br>
2022-04-24 06:07:41 -0300

    readthedocs conf

---
+++ tests/test_callisto.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""Suite de testes para o módulo callisto.
+
+@Author: Luciano Barosi
+@Date: 24.04.2022
+@Links: https://github.com/lbarosi/callisto
+
+"""
+#----------------------------
+#----------------------------
+__license__ = "MIT"
+__version__ = "1.0.1"
+__maintainer__ = "Luciano Barosi"
+__email__ = "lbarosi@df.ufcg.edu.br"
+__status__ = "Development"
+# ----------------------------
+# ----------------------------import os
+import pytest
+os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir))
+import callisto
